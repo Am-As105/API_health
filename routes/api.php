a4e1b3e944811e0db79b65fe62711fae73ee4720
amine <aminear220@gmail.com>
2026-03-17 11:06:25 +0000

    feat : add  auth  routes  ( login , register  , me , logout )

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,25 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+ 
+
+Route::post('/register', [AuthController::class , 'Register']);
+ Route::post('/login', [AuthController::class , 'Login']);
+Route::middleware('auth:sanctum')->group(function(){
+
+ 
+    Route::post('/Logout', [AuthController::class , 'Logout']);
+    Route::get('/me', [AuthController::class , 'me']);
+
+
+    
+
+});
