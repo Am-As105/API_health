207b48f2e26447ea0aa7e4ef7b6af3ddc439ae3e
amine <aminear220@gmail.com>
2026-03-27 10:01:46 +0100

    Add index and store to Appointment controller

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -2,9 +2,58 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Appointment;
 use Illuminate\Http\Request;
 
 class AppointmentController extends Controller
 {
-    //
+    public function index(Request $request )
+    {
+        $appointments = Appointment::with('doctor')
+            ->where('user_id', $request->user()->id)
+            ->get();
+             return response()->json([
+            'success' => true,
+            'data' => $appointments,
+            'message' => 'List of appointments'
+        ]);
+    }
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'doctor_id' => 'required|exists:doctors,id',
+            'appointment_date' => 'required|date|after:now',
+            'notes' => 'nullable|string',
+        ]);
+
+        $appointment = Appointment::create([
+            'user_id' => $request->user()->id,
+            'doctor_id' => $validated['doctor_id'],
+            'appointment_date' => $validated['appointment_date'],
+            'notes' => $validated['notes'] ?? null,
+            'status' => 'pending'
+        ]);
+        return response()->json([
+            'success' => true,
+            'data' => $appointment,
+            'message' => 'Appointment created'
+        ]);
+    }
+    public function show (Request $request , $id)
+    {
+        $appointment = Appointment::with('doctor')
+            ->where('id', $id)
+            ->where('user_id', $request->user()->id)
+            ->first();
+
+        if (!$appointment)
+        {
+            return response()->([
+                
+
+                
+            ])
+        }
+
+    }
 }
