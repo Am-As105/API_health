fd7998320f5d1b51a61db365ab256679fcc60e71
amine <aminear220@gmail.com>
2026-03-27 09:58:50 +0100

    Add relations to AiAdvice model

--- app/Models/AiAdvice.php
+++ app/Models/AiAdvice.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use App\Models\Symptom;
 use Illuminate\Database\Eloquent\Model;
 
 class AiAdvice extends Model
 {
-    //
+    public function user()
+{
+    return $this->belongsTo(User::class);
+}
+
+public function symptoms()
+{
+    return $this->belongsToMany(Symptom::class);
+}
 }
