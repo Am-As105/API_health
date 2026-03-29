e162aac2fa84a23e22ec944863b13a40587580e0
amine <aminear220@gmail.com>
2026-03-29 23:16:23 +0100

    feat(doctors): simple DoctorController with index, show, search

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -2,10 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Doctor;
 use Illuminate\Http\Request;
 
 class DoctorController extends Controller
 {
-    
-    
+    public function index()
+    {
+        $doctors = Doctor::all();
+        return response()->json(['success'=>true,'data'=>$doctors,'message'=>'ok']);
+    }
+
+    public function show($id)
+    {
+        $doctor = Doctor::find($id);
+        return response()->json(['success'=>true,'data'=>$doctor,'message'=>'ok']);
+    }
+
+    public function search(Request $request)
+    {
+        $query = Doctor::query();
+        if($request->has('specialty')) $query->where('specialty',$request->specialty);
+        if($request->has('city')) $query->where('city',$request->city);
+        $doctors = $query->get();
+        return response()->json(['success'=>true,'data'=>$doctors,'message'=>'ok']);
+    }
 }
